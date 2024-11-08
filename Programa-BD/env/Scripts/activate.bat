8c1a9b38e8b507207aae0f0383135e4b7b73d56d
diego-reb <dzgarcia10@gmail.com>
2024-11-08 11:26:59 -0600

    cambios realizados y listos para ingresar base de datos

--- Programa-BD/env/Scripts/activate.bat
+++ Programa-BD/env/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\Dell\Proyecto\env
+set VIRTUAL_ENV=C:\Users\Dell\CodeUp\Programa-BD\env
 
 if not defined PROMPT set PROMPT=$P$G
 
