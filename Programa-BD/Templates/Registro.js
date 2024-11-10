634af4b0d1680c497b8e254cce9a3d2c1248f626
diego-reb <dzgarcia10@gmail.com>
2024-11-10 00:58:40 -0600

    prueba conectar back y front

---
+++ Programa-BD/Templates/Registro.js
@@ -0,0 +1,6 @@
+var form = 
+document.getElementById('registroForm');
+form.addEventListener('submit', function(event) {
+    console.log('hola mundo');
+
+});
--- Programa-BD/env/conexion.py
+++ Programa-BD/env/conexion.py
@@ -4,20 +4,22 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from sqlalchemy import Enum
 
+
 app = Flask(__name__)
 
 # Configuración de la base de datos
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:@127.0.0.1/BD' 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:@localhost:5432/CodeUp'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 
-class Usuarios(db.Model):
+
+class Usuario(db.Model):
     id_usuario = db.Column(db.Integer, primary_key=True)
     nombre = db.Column(db.String(100), nullable=False)
     contraseña = db.Column(db.String(100), unique=True, nullable=False)
-    tipo_usuario = db.Column(db.String(50), nullable=False)
+    id_tipo_de_usuario = db.Column(db.String(50), nullable=False)
 
 class Cursos(db.Model):
     id_curso = db.Column(db.Integer, primary_key=True)
